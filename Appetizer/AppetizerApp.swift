b0af1116a02587d347f49c1ffca56f53927ff61f
hashimsaffarini <hashimsaffarini044@gmail.com>
2026-01-24 21:28:38 +0200

    Initial Commit

---
+++ Appetizer/AppetizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizerApp.swift
+//  Appetizer
+//
+//  Created by Hashim Saffarini on 24/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct AppetizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
