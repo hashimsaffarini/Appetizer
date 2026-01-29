5194d314c90982c99f696b383e88f472d25b8b26
hashimsaffarini <hashimsaffarini044@gmail.com>
2026-01-29 18:54:05 +0200

    feat : setup ui for  account view

--- Appetizer/Screens/AccountView/AccountView.swift
+++ Appetizer/Screens/AccountView/AccountView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var date = Date()
+    @State private var extraNapkins = false
+    @State private var frequentRefills = false
+    
     var body: some View {
-        Text("Account View")
-            .navigationTitle("👤 Account")
+        Form{
+            Section(header : Text("Personal Info")){
+                TextField("First Name", text: $firstName)
+                TextField("Last Name", text: $lastName)
+                TextField("Email", text: $email)
+                    .keyboardType(.emailAddress)
+                    .textInputAutocapitalization(.none)
+                    .autocorrectionDisabled(true)
+                DatePicker("Birthday", selection: $date, displayedComponents: .date)
+            }
+            Section(header : Text("Requests")){
+                Toggle("Extra Napkins", isOn: $extraNapkins)
+                Toggle("Frequent Refills", isOn: $frequentRefills)
+            }
+            .tint(.accentColor)
+            
+        }
+        .navigationTitle("👤 Account")
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button("Save") {
+                    print("Saved")
+                }
+            }
+        }
     }
 }
 
